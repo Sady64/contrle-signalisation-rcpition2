f9764529a23bf853e9af2e09258e97f4a50ea887
Sady64 <83607769+Sady64@users.noreply.github.com>
2021-06-04 11:38:14 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == gauche) {
         for (let index = 0; index < 3; index++) {
-            left.showColor(neopixel.colors(NeoPixelColors.Red))
+            left.showColor(neopixel.colors(NeoPixelColors.Yellow))
             basic.showLeds(`
                 . . # . .
                 . # . . .
@@ -9,7 +9,7 @@ radio.onReceivedNumber(function (receivedNumber) {
                 . # . . .
                 . . # . .
                 `)
-            basic.pause(300)
+            basic.pause(100)
             left.showColor(neopixel.colors(NeoPixelColors.Black))
             basic.showLeds(`
                 . . . . .
@@ -18,7 +18,7 @@ radio.onReceivedNumber(function (receivedNumber) {
                 . . . . .
                 . . . . .
                 `)
-            basic.pause(300)
+            basic.pause(100)
         }
     }
 })
@@ -26,9 +26,8 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(gauche)
 })
 radio.onReceivedString(function (receivedString) {
-    droite += droite + 1
     for (let index = 0; index < 3; index++) {
-        right.showColor(neopixel.colors(NeoPixelColors.Red))
+        right.showColor(neopixel.colors(NeoPixelColors.Yellow))
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -36,7 +35,7 @@ radio.onReceivedString(function (receivedString) {
             . . . # .
             . . # . .
             `)
-        basic.pause(300)
+        basic.pause(100)
         right.showColor(neopixel.colors(NeoPixelColors.Black))
         basic.showLeds(`
             . . . . .
@@ -45,40 +44,17 @@ radio.onReceivedString(function (receivedString) {
             . . . . .
             . . . . .
             `)
-        basic.pause(300)
-        droite += droite - 1
+        basic.pause(100)
     }
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendString("DROITE")
 })
-let z3 = 0
-let z2 = 0
-let z1 = 0
 let right: neopixel.Strip = null
 let left: neopixel.Strip = null
 let gauche = 0
-let droite = 0
 radio.setGroup(33)
-droite = 0
 gauche = 0
 let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
-left = strip.range(12, 24)
-right = strip.range(0, 12)
-basic.forever(function () {
-    z1 = input.acceleration(Dimension.Z)
-    basic.pause(10)
-    z2 = input.acceleration(Dimension.Z)
-    basic.pause(10)
-    z3 = input.acceleration(Dimension.Z)
-    if (z1 < z2) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(3000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    }
-    if (z3 < z2) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(3000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    }
-})
+left = strip.range(12, 20)
+right = strip.range(0, 13)
